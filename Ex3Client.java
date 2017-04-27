ec57cb53cfcce3c7c2b96ec2b6e7c0460bd4b0cf
Christopher <rivas1@cpp.edu>
2017-04-27 16:05:02 -0700

    Using InputStream only to obtain number of bytes to be read.
    Removed the use of InputStreamReader, and BufferedReader to obtain value
    for number of bytes. Appears to be working properly.

--- Ex3Client.java
+++ Ex3Client.java
@@ -5,42 +5,40 @@ public final class Ex3Client
 {
 	public static void main (String[] args) throws IOException
 	{
-		int numberOfBytes = -1; // stores number of bytes to be received [0,255]
+		// int numberOfBytes = -1; // stores number of bytes to be received [0,255]
+		int n = -1;
 		try
 		{
-			Socket socket = new Socket ("codebank.xyz", 38103); // make a connection
+			/* make a connection */
+			Socket socket = new Socket ("codebank.xyz", 38103); 
 			if (socket.isConnected())
 				System.out.println("Connected to server.");	
 
-			// read in byte that contains number of bytes to be received
-			InputStream IS 			= socket.getInputStream();
-			InputStreamReader ISR 	= new InputStreamReader(IS, "UTF-8");
-			BufferedReader BR 		= new BufferedReader(ISR);
+			/* create stream from Socket */
+			InputStream IS = socket.getInputStream();
 
-			numberOfBytes = obtain(socket, IS, ISR, BR);
-			System.out.println("Reading " + numberOfBytes + " bytes.");
+			/* read in byte that contains number of bytes to be received */
+			n = obtain(socket, IS);
+			System.out.println("Reading " + n + " bytes."); 
 
-			// If not between 0 and 255, quit.
-			if ( numberOfBytes > 255 || numberOfBytes < 0 )
+			/* If not between 0 and 255, quit. */
+			if ( n > 255 || n < 0 )
 			{
 				System.out.println("Value is not between 0 and 255!\nProgram will now terminate.");
 				System.exit(0);
 			}
-
-			// 
 		}
 		catch (IOException e)
 		{ e.printStackTrace(); }
 	}
-	public static int obtain ( Socket socket, InputStream IS, InputStreamReader ISR, 
-							   BufferedReader BR ) throws IOException
+	public static int obtain ( Socket socket, InputStream IS ) throws IOException
 	{
 		try 
 		{
-			return BR.read();
+			return IS.read();
 		}
 		catch (IOException e)
 		{ e.printStackTrace(); }
-		return BR.read();
+		return IS.read();
 	}
 }
